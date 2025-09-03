a25655218375c8ea3042ccbbc40da186aa25a565
TomasHoffer2002 <tomihoffer2002@gmail.com>
2025-09-03 11:01:07 -0300

    main

---
+++ src/main/java/ar/edu/unlpam/ing/Grupo_4_tp_Spring/Grupo4TpSpringApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.Grupo_4_tp_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo4TpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo4TpSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlpam/ing/Grupo_4_tp_Spring/Grupo4TpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.Grupo_4_tp_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo4TpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
